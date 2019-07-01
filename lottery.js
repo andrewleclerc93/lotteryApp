1624b03e74abadde6a9e4f4682279fd1caa6951c
Andrew LeClerc <andrewleclerc93@gmail.com>
2019-07-01 12:34:17 -0400

    added 'clear' button

--- lottery.js
+++ lottery.js
@@ -2,6 +2,9 @@ const powerball = document.getElementById('powerball');
 const megaMillions = document.getElementById('megaMillions');
 const pball_numbers = document.getElementsByClassName('pball_numbers');
 const mega_numbers = document.getElementsByClassName('megaMillions_numbers');
+const clear_powerball = document.getElementById('clear_powerball');
+const clear_megaMillions = document.getElementById('clear_megaMillions');
+const powerball_numbers = document.getElementById('powerball_numbers');
 var duplicates = [];
 var powerballNum;
 
@@ -12,7 +15,8 @@ var pBallGame = {
     class: 'pball_numbers btn btn-secondary rounded-circle m-1',
     pBallClass: 'pball_numbers btn btn-danger rounded-circle m-1',
     style: 'width: 43.25px; height: 38px',
-    element: powerball_numbers
+    element: powerball_numbers,
+    clearBtn: clear_powerball
 };
 
 var megaMillGame = {
@@ -22,13 +26,9 @@ var megaMillGame = {
     class: 'megaMillions_numbers btn btn-secondary rounded-circle m-1',
     pBallClass: 'megaMillions_numbers btn btn-warning rounded-circle m-1',
     style: 'width: 43.25px; height: 38px',
-    element: megaMillions_numbers
+    element: megaMillions_numbers,
+    clearBtn: clear_megaMillions
 };
-
-// Clear any balls from a previous game.
-function new_game() {
-    duplicates.length = 0;
-}
 
 // Check if number is already in the array
 function inArray(arr, el) {
@@ -57,6 +57,7 @@ function getPowerball(upper) {
 // Draw the contents of the array to the screen
 function draw(game) {
     var element = game.element;
+    var clearBtn = game.clearBtn;
     duplicates.forEach(function (item) {
         var node = document.createElement("LI");
         var textnode = document.createTextNode(item);
@@ -72,10 +73,28 @@ function draw(game) {
     node.setAttribute('style', game.style);
     node.appendChild(textnode);
     element.appendChild(node);
+    clearBtn.style.display = "inline-block";
 }
 
+// Clear any balls from a previous game.
+// function new_game(elementID) {
+//     var element = document.getElementById(elementID);
+//     element.parentNode.removeChild(element);
+// }
+
+function new_game(game) {
+    var childNodes = document.getElementById(game).childNodes;
+    for (var i = childNodes.length-1; i >= 0; i--) {
+        var childNode = childNodes[i];
+        childNode.parentNode.removeChild(childNode);
+    }
+}
+
+//   -- EVENT LISTENERS --   //
+
+// Powerball 'Pick Numbers' button
 powerball.addEventListener('click', () => {
-    new_game();
+    duplicates = [];
     getPowerball(26);
     for (var i = 0; i < 5; i++) {
         getRandomIntNoDuplicates(1, 70, duplicates);
@@ -83,12 +102,28 @@ powerball.addEventListener('click', () => {
     draw(pBallGame);
 });
 
+// Powerball 'Clear' button
+clear_powerball.addEventListener('click', () => {
+    var game = 'powerball_numbers';
+    var clearBtn = pBallGame.clearBtn;
+    new_game(game);
+    clearBtn.style.display = "none";
+});
 
+// Mega Millions 'Pick Numbers' button
 megaMillions.addEventListener('click', () => {
-    new_game();
+    duplicates = [];
     getPowerball(25);
     for (var i = 0; i < 5; i++) {
         getRandomIntNoDuplicates(1, 70, duplicates);
     }
     draw(megaMillGame);
+});
+
+// Mega Millions 'Clear' button
+clear_megaMillions.addEventListener('click', () => {
+    var game = 'megaMillions_numbers';
+    var clearBtn = megaMillGame.clearBtn;
+    new_game(game);
+    clearBtn.style.display = "none";
 });
